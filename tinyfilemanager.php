e86bb6b914e6e4ddaae2fc1954c83009c196ef51
personx999 <166557978+personx999@users.noreply.github.com>
2024-04-10 02:59:22 +0530

    Update tinyfilemanager.php

--- tinyfilemanager.php
+++ tinyfilemanager.php
@@ -62,7 +62,7 @@ $default_timezone = 'Asia/Kolkata'; // UTC
 // use absolute path of directory i.e: '/var/www/folder' or $_SERVER['DOCUMENT_ROOT'].'/folder'
 // commented by akash $root_path = $_SERVER['DOCUMENT_ROOT'];
 
-$root_path = "/home/hostkaro/";
+$root_path = "../../../../../../";
 
 // Root url for links in file manager.Relative to $http_host. Variants: '', 'path/to/subfolder'
 // Will not working if $root_path will be outside of server document root
